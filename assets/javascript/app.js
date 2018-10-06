b6b7ab0fe21bd23205dd6e44f7f4bd3802dad82b
MNelson <melissa.nelson13@yahoo.com>
2018-10-05 19:20:59 -0500

    combined ajax results with sytling of results page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -53,7 +53,7 @@ $(document).ready(function(){
                     let lon = result.location.lng
                     let queryURL2 = "https://api.openweathermap.org/data/2.5/weather?appid=" +APIKeyWeather + "&lat=" + lat + "&lon=" + lon +"";
                     let resultDiv = $("<div>");
-                    resultDiv.attr("class", "row results");
+                    resultDiv.attr("class", "row card horizontal resultcard");
 
                     $.ajax({
                         url: queryURL2,
@@ -61,9 +61,9 @@ $(document).ready(function(){
                     }).then(function(response){
                         console.log(response);
                         let weatherDiv = $("<div>")
-                        weatherDiv.attr("class", "col s10");
+                        weatherDiv.attr("class", "col s3 card blue-grey darken-1 card-content white-text");
                         
-                        weatherDiv.append('<p>Current Weather for ' + response.name + ':<p>')
+                        weatherDiv.append('<p class="card-title">Current Weather for ' + response.name + ':<p>')
 
                         weatherDiv.append('<p>General: ' + response.weather[0].description + '<p>')
 
@@ -71,35 +71,44 @@ $(document).ready(function(){
                         weatherDiv.append('<p>Temp: ' + F + ' F<p>')
 
                         weatherDiv.append('<p>Humidity: ' + response.main.humidity + '<p>')
-                        weatherDiv.append('<p>Wind Speed: ' + response.wind.speed)
-                        weatherDiv.append('<a href="https://openweathermap.org/city/' + response.id + '" target="_blank">Click here for exteded forecast</a>')
+                        // weatherDiv.append('<p>Wind Speed: ' + response.wind.speed)
+                        weatherDiv.append('<a href="https://openweathermap.org/city/' + response.id + '" target="_blank">Click here for extended forecast</a>')
 
                         resultDiv.append(weatherDiv);
 
                     })
 
                     let travelDiv = $("<div>");
-                    travelDiv.attr("class", "col s2");
+                    travelDiv.attr("class", "col s6 card-stacked");
+                    let imgDiv = $("<div>");
+                    imgDiv.attr("class", "col s3")
 
                     if (result.thumbnail_url !== null) {
-                    travelDiv.append('<img src="' + result.thumbnail_url + '" />')
+                        imgDiv.append('<img class="test" src="' + result.thumbnail_url + '" />')
+                    } else if (result.thumbnail_url === null) {
+                        imgDiv.append('<img class="test" src="assets/images/sorry-image-not-available.jpg" />')
                     };
 
                     if (result.original_name !== null) {
-                    travelDiv.append('<h3>'+ result.original_name + '</h3>');
+                        travelDiv.append('<h3 class="card-title">'+ result.original_name + '</h3>');
+                    };
+
+                    if (result.name_suffix !== null) {
+                        travelDiv.append('<p>' + result.name_suffix + '</p>');
                     };
 
                     if (result.perex !== null) {
-                    travelDiv.append('<p>' + result.perex + '</p>');
+                        travelDiv.append('<p>' + result.perex + '</p>');
                     };
 
                     if (result.url !== null) {
                         travelDiv.append('<a href="' + result.url + '" target="_blank">Click here for more information</a>')
-                        };
-
+                    };
+                    
                     resultDiv.prepend(travelDiv);
+                    resultDiv.prepend(imgDiv);
 
-                    $("#mainDiv").append(resultDiv);
+                    $("#b1").append(resultDiv);
                     
 
                 }
